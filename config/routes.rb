4ffff79e142480151a540a956b2e15f1f6ffbf34
Aline <alinecris@gmail.com>
2020-11-03 21:44:46 +0100

    Configure API authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth/v1/user'
-  as :user do
-    # Define routes for User within this block.
+
+  namespace :admin do
+    namespace :v1 do
+      get "home" => "home#index"
+    end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :storefront do
+    namespace :v1 do
+
+    end
+  end
 end
