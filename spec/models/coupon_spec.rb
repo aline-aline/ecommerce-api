09535f975b2b881ea77b89cd15bd9a7138c67a2d
Aline <alinecris@gmail.com>
2020-11-10 22:10:06 +0100

    Add date validate to coupon model

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -8,4 +8,22 @@ RSpec.describe Coupon, type: :model do
   it { is_expected.to validate_presence_of(:due_date) }
   it { is_expected.to validate_presence_of(:discount_value) }
   it { is_expected.to validate_numericality_of(:discount_value).is_greater_than(0) }
+
+  it "due_date can't be a past date" do
+    subject.due_date = 1.day.ago
+    subject.valid?
+    expect(subject.errors.keys).to include :due_date
+  end
+
+  it "due_date can't be a current date" do
+    subject.due_date = Time.zone.now
+    subject.valid?
+    expect(subject.errors.keys).to include :due_date
+  end
+
+  it "is valid with future date" do
+    subject.due_date = Time.zone.now + 1.hour
+    subject.valid?
+    expect(subject.errors.keys).to_not include :due_date
+  end
 end
