c67a39d6ac33a1e59839dda35e1b18b799fc2d2d
Aline <alinecris@gmail.com>
2020-11-26 21:15:50 +0100

    Add status to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,9 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :image, presence: true
+  validates :status, presence: true
+
+  enum status: { available: 1, unavailable: 2 }
 
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
